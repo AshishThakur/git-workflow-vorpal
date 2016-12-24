0d3d50140412c4ce4d20cda572d83b64941a69b2
Ashish Thakur <dash2ashish@gmail.com>
2016-12-24 15:28:57 +0530

    Added multiple options

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 var vorpal = require('vorpal')();
+var exec = require('child_process').exec;
 
 vorpal
     .delimiter('git-workflow$')
@@ -8,12 +9,19 @@ vorpal
     .command('feature start', 'Start working on a new feature')
     .action(function (args, cb) {
       const self = this;
-      this.prompt({
-        type: 'input',
-        name: 'ticket-id',
-        message: 'Enter the ticket ID of the story:',
-        // Others prompt, the base branch + titlte of the ticket
-      }, function (result) {
+      this.prompt([
+          {
+            type: 'input',
+            name: 'ticket-id',
+            message: 'Enter the ticket ID of the story:',
+            // Others prompt, the base branch + titlte of the ticket
+          },
+          {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of the story',
+          }],
+       function (result) {
         // Create a function to create a new branch: ticket-id-<title>
         // where ticket-id is the ticket id of your story and <title> is
         // title of your story lowercase + spaces replaced by '-' and max
@@ -33,6 +41,14 @@ vorpal
 
 vorpal
   .command('feature checkout', 'Checkout to an existing feature branch')
+  .option('-b, --branch <branch>')
+  .action(function (args, callback) {
+    if (typeof args.options.branch == 'undefined') {
+      this.log('No options!');
+      callback();
+    }
+    callback();
+  });
 
 vorpal
   .command('feature rebase')
@@ -41,14 +57,16 @@ vorpal
   .command('feature publish')
   .option('-r, --remote <remote>')
   .option('-b, --branch <branch>')
-  .action(function (args, callback) {
+  .validate(function (args) {
     if (typeof args.options.remote === 'undefined' || typeof args.options.branch == 'undefined') {
-      this.log('No options!');
-      callback();
+      this.help();
+      return 'No options!';
     }
+  })
+  .action(function (args, callback) {
+    exec
     callback();
   });
 
 vorpal
   .command('release tag', 'Release a tag')
-
