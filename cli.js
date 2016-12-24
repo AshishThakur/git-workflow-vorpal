c57e8af73eb31c94f4531eb1175c8ef2a6770222
Ashish Thakur <dash2ashish@gmail.com>
2016-12-24 12:07:36 +0530

    Added prompts

---
+++ cli.js
@@ -0,0 +1,30 @@
+var vorpal = require('vorpal')();
+
+vorpal
+    .delimiter('git-workflow$')
+    .show();
+
+vorpal
+    .command('feature start', 'Start working on a new feature')
+    .action(function (args, cb) {
+      const self = this;
+      this.prompt({
+        type: 'input',
+        name: 'ticket-id',
+        message: 'Enter the ticket ID of the story:',
+        // Others prompt, the base branch + titlte of the ticket
+      }, function (result) {
+        // Create a function to create a new branch: ticket-id-<title>
+        // where ticket-id is the ticket id of your story and <title> is
+        // title of your story lowercase + spaces replaced by '-' and max
+        // 10 chars. Create a function for this
+        // git checkout -b <feature-branch> <base>
+
+      });
+    });
+
+vorpal
+  .command('feature finish')
+
+vorpal
+  .command('feature publish')
